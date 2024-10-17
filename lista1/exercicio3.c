764f3980ff6fd911d9a9a352259a08b9c63b54fa
cairuga <62620754+cairuga@users.noreply.github.com>
2024-10-17 10:50:55 -0300

    Update exercicio3.c

--- lista1/exercicio3.c
+++ lista1/exercicio3.c
@@ -2,18 +2,18 @@
 
 int main() {
 
-    float basico, valorM3, consumo, tarifa;
+    float basico, valor_metro, consumo, tarifa;
     
     printf("Informe o valor do serviço básico: ");
     scanf("%f", &basico);
 
     printf("Informe o valor do metro cúbico de água: ");
-    scanf("%f", &valorM3);
+    scanf("%f", &valor_metro);
 
     printf("Informe o consumo (m3) de água: ");
     scanf("%f", &consumo);
 
-    tarifa = basico + (consumo * valorM3);
+    tarifa = basico + (consumo * valor_metro);
     
     printf("A tarifa de água do usuário é %.2f", tarifa);
 
