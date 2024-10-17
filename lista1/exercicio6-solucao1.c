5804f103f91501d6610b9c52dc576452c04b5db5
cairuga <62620754+cairuga@users.noreply.github.com>
2024-10-17 11:05:46 -0300

    Create exercicio6-solucao1.c

---
+++ lista1/exercicio6-solucao1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    float numero1, numero2;
+    
+    printf("Informe o primeiro número: ");
+    scanf("%f", &numero1);
+
+    printf("Informe o segundo número: ");
+    scanf("%f", &numero2);
+
+    if (numero1 > numero2) {
+        printf("O maior número digitado é %f ", numero1);
+    } else {
+        printf("O maior número digitado é %f ", numero2);
+    }
+
+    /*    
+    Como são apenas dois valores, esta é uma solução simples. 
+    Contudo, a partir de três valores, é mais indicado utilizar uma variável auxiliar. 
+    Veja outra solução no arquivo exercicio6-solucao2.c
+    */
+    
+    return 0;
+}
