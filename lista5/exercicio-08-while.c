f9e3c77244c8d5392700e3ee8448ae60709a4990
cairuga <62620754+cairuga@users.noreply.github.com>
2024-11-28 17:18:21 -0300

    Update exercicio-08-while.c

--- lista5/exercicio-08-while.c
+++ lista5/exercicio-08-while.c
@@ -2,6 +2,31 @@
 
 int main() {
 
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    while (numero != 0) {
+    
+        if (numero < 0) {
+            printf("NEGATIVO\n");
+        } else if (numero > 0) {
+            printf("POSITIVO\n");
+        } 
+        
+        printf("Digite um numero: ");
+        scanf("%d", &numero);
+    }
+    return 0;
+}
+
+/* ALTERNATIVA 
+
+#include <stdio.h>
+
+int main() {
+
     int numero = 1;
 
     while (numero != 0) {
@@ -16,3 +41,5 @@ int main() {
     }
     return 0;
 }
+
+*/
