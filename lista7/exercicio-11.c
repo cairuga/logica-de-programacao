1f449b64f90ec03f01e822066f26f7b81fab72ba
cairuga <62620754+cairuga@users.noreply.github.com>
2025-01-05 15:36:09 -0300

    Create exercicio-11.c

---
+++ lista7/exercicio-11.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+    int i, qtd_alunos;
+    float maior, menor, soma, media;
+
+    printf("Informe a quantidade de alunos: ");
+    scanf("%d", &qtd_alunos);
+
+    float notas[qtd_alunos];
+
+    //Lendo o vetor
+    for (i = 0; i < qtd_alunos; i++) {
+        printf("Digite a nota: ");
+        scanf("%f", &notas[i]);
+    }
+
+    printf("Notas dos aprovados: ");
+    for (i = 0; i < qtd_alunos; i++) {
+        if (notas[i] >= 6) {
+            printf("%.1f ", notas[i]);
+        }
+    }
+
+    printf("\nNotas dos reprovados: ");
+    for (i = 0; i < qtd_alunos; i++) {
+        if (notas[i] < 6) {
+            printf("%.1f ", notas[i]);
+        }
+    }
+
+    return 0;
+}
