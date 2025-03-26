47d1bd82317bd38eae61ab2ff03e8e9a60ce903d
cairuga <62620754+cairuga@users.noreply.github.com>
2025-03-26 13:18:28 -0300

    Create exercicio-02.c

---
+++ lista-01/exercicio-02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	printf("# Frutas e Verduras:\n");
+	printf("\t- Banana\n");
+	printf("\t- Mamão\n");
+	printf("\t- Ameixa\n");
+	printf("\t- Rúcula\n");
+	printf("\n");
+	printf("# Higiene e Limpeza:\n");
+	printf("\t- Detergente\n");
+	printf("\t- Creme dental\n");
+	printf("\t- Desinfetante\n");
+	printf("\t- Esponja\n");
+	printf("\t- Sabonete líquido\n");
+}
