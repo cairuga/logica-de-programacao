83073797569b606f7f7b0b0e42e890f675462950
cairuga <62620754+cairuga@users.noreply.github.com>
2025-03-26 13:55:49 -0300

    Update exercicio-03.c

--- lista-01/exercicio-03.c
+++ lista-01/exercicio-03.c
@@ -7,7 +7,7 @@ int main() {
 	printf("Eu tenho %d anos.\n", idade);
 	printf("Minha altura é %.2f m.\n", altura);
 	printf("Eu tenho %d anos e minha altura é %.2f m.\n", idade, altura);
-	printf("Minha altura é %.2f e eu tenho %d anos.\n", altura, idade);
+	printf("Minha altura é %.2f e eu tenho %d anos.", altura, idade);
 	
 	return 0;
 }
