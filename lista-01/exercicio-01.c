d10146b10684c2fc14dacf76bb02635ff682dd9b
cairuga <62620754+cairuga@users.noreply.github.com>
2025-03-26 13:29:53 -0300

    Create exercicio-01.c

---
+++ lista-01/exercicio-01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+	printf("\n\nletra a)\n");
+	printf("Lógica de Programação");
+
+	printf("\n\nletra b)\n");
+	printf("Lógica ");
+	printf("de ");
+	printf("Programação");
+	
+	printf("\n\nletra c)\n");
+	printf("Lógica\nde\nProgramação");
+		
+	printf("\n\nletra d)\n");
+	printf("Lógica\n");
+	printf("de\n");
+	printf("Programação");
+	
+	printf("\n\nletra e)\n");
+	printf("Lógica\n\nde\n\nProgramação");
+
+	printf("\n\nletra f)\n");
+	printf("Lógica\n");
+	printf("\n");
+	printf("de\n");
+	printf("\n");
+	printf("Programação");
+	
+	return 0;
+}
